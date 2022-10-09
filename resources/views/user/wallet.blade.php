ce405f5dfb563ba5c427ecff5c5ce646ca869575
Phonixoop <33152793+Phonixoop@users.noreply.github.com>
2022-10-09 09:33:26 +0330

    first commit

---
+++ resources/views/user/wallet.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.user') 
+@section('header')
+@endsection
+@section('content')
+
+<div class="page-body">
+  <div class="container-fluid">
+    <div class="page-title">
+      <div class="row">
+          <div class="col-6">
+              <h3>کیف پول</h3>
+          </div>
+          <div class="col-6">
+              <ol class="breadcrumb">
+                  <li class="breadcrumb-item">
+                      <a href="/">
+                          <i data-feather="home"></i
+                      ></a>
+                  </li>
+                  <li class="breadcrumb-item">کیف پول</li>
+              </ol>
+          </div>
+      </div>
+  </div>
+  </div>
+  <!-- Container-fluid starts-->
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-12">
+        
+      </div>
+    </div>
+  </div>
+  <!-- Container-fluid Ends-->
+</div>
+@endsection
+@section('footer')
+
+@endsection
