4c59eb2bc92c0378b0bef1ce7373a4dea23bd3e8
Phonixoop <33152793+Phonixoop@users.noreply.github.com>
2022-10-12 20:35:24 +0330

    transaction view- no transactions img added

--- resources/views/user/transactions.blade.php
+++ resources/views/user/transactions.blade.php
@@ -55,62 +55,67 @@
   <!-- Container-fluid starts-->
   <div class="container-fluid">
     <div class="row">
-      <div class="col-sm-6" style="padding:0px; text-align: center; border-radius: 20px; !important; margin:0 auto; background-color: #fcffff;overflow:hideen; ">
-        @if(isset($transactions))
-        <table dir="rtl" class="table">
-            <thead class="thead-dark">
-              <tr class="">
-                <th scope="col">#</th>
-                <th scope="col  ">
-                    <div class=" flex justify-center items-center">تاریخ</div>
-                </th>
-                <th scope="col   ">
-                    <div class=" flex justify-center items-center">مقدار پرداختی</div>
-                </th>
-                <th scope="col ">
-                    <div class=" flex justify-center items-center">وضعیت پرداخت</div>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-                @foreach($transactions as $key=>$transaction)
-                @php $rowStyle = $key % 2 === 0 ? "background-color: #e1eeef; border-radius:20px;" : "background-color: #ffffff" @endphp
-                <tr style="{{$rowStyle}}">
-                    @php $date = \Morilog\Jalali\Jalalian::fromCarbon(\Carbon\Carbon::parse($transaction["date"])); @endphp
-                    <th scope="row">{{$key + 1}}</th>
-                    <td >
-                        <div class="flex justify-center items-center gap-3">
-                            <span style="padding: 2px">{{$date->format("d M Y")}}</span>
-                          
-                            <span  style="padding: 2px">{{$date->format("H:i:s")}}</span>
-                        </div>
-                    </td>
-                    <td >
-                      @php $amount = preg_replace("/\B(?=(\d{3})+(?!\d))/",",",$transaction["amount"]); @endphp 
-                        <div class="flex justify-center items-center">{{$amount}} تومان</div>
-                    </td>
-                    <td  class="flex justify-center items-center">
-                 
-                        @if($transaction["hasPayed"])
-                        <div class="flex justify-center items-center" style="border: #199EA3 1px solid; width: 100px; color:#199EA3; text-align: center; border-radius: 100px; padding:5px 10px;">
-                            موفق
-                        </div>
-                        @else 
-                        <div class="flex justify-center items-center"  style="border: #e93939 1px dashed; color:#e93939; width: 100px; text-align: center; border-radius: 100px; padding:5px 10px;">
-                            نا موفق
-                        </div>
-                        @endif     
-                    </td>
-                  </tr>
-                @endforeach
-        
-            </tbody>
-          </table>
-          @else
-            تراکنشی وجود ندارد
-          @endif
-        
-        </div>
+   
+          @if(isset($transactions) && false)
+          <div class="col-sm-6" style="padding:0px; text-align: center; border-radius: 20px; !important; margin:0 auto; background-color: #fcffff;overflow:hideen; ">
+          <table dir="rtl" class="table">
+              <thead class="thead-dark">
+                <tr class="">
+                  <th scope="col">#</th>
+                  <th scope="col  ">
+                      <div class=" flex justify-center items-center">تاریخ</div>
+                  </th>
+                  <th scope="col   ">
+                      <div class=" flex justify-center items-center">مقدار پرداختی</div>
+                  </th>
+                  <th scope="col ">
+                      <div class=" flex justify-center items-center">وضعیت پرداخت</div>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                  @foreach($transactions as $key=>$transaction)
+                  @php $rowStyle = $key % 2 === 0 ? "background-color: #e1eeef; border-radius:20px;" : "background-color: #ffffff" @endphp
+                  <tr style="{{$rowStyle}}">
+                      @php $date = \Morilog\Jalali\Jalalian::fromCarbon(\Carbon\Carbon::parse($transaction["date"])); @endphp
+                      <th scope="row">{{$key + 1}}</th>
+                      <td >
+                          <div class="flex justify-center items-center gap-3">
+                              <span style="padding: 2px">{{$date->format("d M Y")}}</span>
+                            
+                              <span  style="padding: 2px">{{$date->format("H:i:s")}}</span>
+                          </div>
+                      </td>
+                      <td >
+                        @php $amount = preg_replace("/\B(?=(\d{3})+(?!\d))/",",",$transaction["amount"]); @endphp 
+                          <div class="flex justify-center items-center">{{$amount}} تومان</div>
+                      </td>
+                      <td  class="flex justify-center items-center">
+                  
+                          @if($transaction["hasPayed"])
+                          <div class="flex justify-center items-center" style="border: #199EA3 1px solid; width: 100px; color:#199EA3; text-align: center; border-radius: 100px; padding:5px 10px;">
+                              موفق
+                          </div>
+                          @else 
+                          <div class="flex justify-center items-center"  style="border: #e93939 1px dashed; color:#e93939; width: 100px; text-align: center; border-radius: 100px; padding:5px 10px;">
+                              نا موفق
+                          </div>
+                          @endif     
+                      </td>
+                    </tr>
+                  @endforeach
+          
+              </tbody>
+            </table>
+          </div>
+            @else
+            <div class="col-sm-6" style="padding:0px; text-align: center; margin:0 auto;  ">
+             <h3> تراکنشی وجود ندارد</h3>
+              <img src="/main/images/empty_transactions.svg" />
+            </div>
+         @endif
+   
+      
     </div>
   </div>
   <!-- Container-fluid Ends-->
